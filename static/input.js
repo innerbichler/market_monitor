4a30247819fe390c7ce2cdca47c7df2954f2fbc8
alexander_debian <alexander.innerbichler@bernard-gruppe.com>
2024-05-26 00:58:13 +0200

    working input.html

---
+++ static/input.js
@@ -0,0 +1,17 @@
+
+
+function input_beverage(){
+	let product_name = document.getElementById("product_name").value;
+	let company_name = document.getElementById("company_name").value;
+	let product_type = document.getElementById("product_type").value;
+	let package_type = document.getElementById("package_type").value;
+	let price_per_liter = document.getElementById("price_per_liter").value;
+
+	// date will be set by backend
+	console.log(product_name);
+	console.log(company_name);
+	console.log(product_type);
+	console.log(package_type);
+	console.log(price_per_liter);
+
+}
